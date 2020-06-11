1b47e24c61478c3e65e12bbf401845b121211c86
ws@httpf.hu <ws@httpf.hu>
2020-06-12 00:04:33 +0200

    get message

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
+const axios = required('axios')
+
 exports.createApiEndPoints = (app) => {
     app.get('/api/help', (req, res, next) => {
         console.log('help')
@@ -7,7 +9,20 @@ exports.createApiEndPoints = (app) => {
         if (req.body.data) {
             const msg = req.body.data
             console.log(msg)
-            res.send(msg)
+            const messageId = message.id
+            const messageApiUrl = "https://api.ciscospark.com/v1/messages"
+            const botAccessToken = "NmMwYWZiZDgtOWUyYi00MzI0LWJmYmItYjU2ODA4MzY2NDJjYzg2YWRmMDAtZjJj_PF84_4a05e5c1-65cb-4f86-899f-dbcc12a1af24"
+            const config = {
+                headers: { Authorization: `Bearer ${botAccessToken}` }
+            }
+            const axiosInstance = axios.create({
+                baseURL: messageApiUrl,
+                timeout: 1000,
+                headers: {Authorization: `Bearer ${botAccessToken}`}
+              });
+              axiosInstance.get(messageApiUrl + "/" + messageId)
+            .then(response => console.log(response)) 
+
         }
     })
 }

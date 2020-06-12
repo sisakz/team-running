bd4712d2bc3f487883996f5ed55b7858a3e88e59
ws@httpf.hu <ws@httpf.hu>
2020-06-12 09:09:38 +0200

    get personId

--- api/index.js
+++ api/index.js
@@ -11,21 +11,23 @@ exports.createApiEndPoints = (app) => {
             console.log(msg)
             const messageId = msg.id
             res.send(messageId)
-            const messageApiUrl = "https://api.ciscospark.com/v1/messages"
+            const apiUrl =  "https://webexapis.com/v1"
+            const messageApiUrl = `${ApiUrl}/messages`
+            const peopleUrl = `${ApiUrl}/people`
             const botAccessToken = "NmMwYWZiZDgtOWUyYi00MzI0LWJmYmItYjU2ODA4MzY2NDJjYzg2YWRmMDAtZjJj_PF84_4a05e5c1-65cb-4f86-899f-dbcc12a1af24"
-            const config = {
-                headers: { Authorization: `Bearer ${botAccessToken}` }
-            }
             const axiosInstance = axios.create({
                 baseURL: messageApiUrl,
                 timeout: 1000,
                 headers: {Authorization: `Bearer ${botAccessToken}`}
               });
-              axiosInstance.get(messageApiUrl + "/" + messageId)
+            axiosInstance.get(messageApiUrl + "/" + messageId)
             .then(response => {
                 console.log(response)
-                if (response.data.text)  
-                    console.log(response.data.text)
+                if (response.data)  
+                    const messageText = response.data.text
+                    const personId = response.data.personId
+                    console.log(messageText)
+                    console.log(personId)
             }) 
 
         }

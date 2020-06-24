abc4a60a979c2536722569b36d78df919da0c453
Zoltan Sisak <zoltan.sisak@gmail.com>
2020-06-24 20:13:38 +0200

    getBotAnswer 0.4 bugfix

--- db.js
+++ db.js
@@ -9,3 +9,17 @@ exports.initDatabase =  () => {
         'CREATE TABLE IF NOT EXISTS workouts (id INTEGER PRIMARY KEY, userId INTEGER, distance REAL, time INTEGER, createdAt DATETIME, updatedAt DATETIME);'
     )
 }
+
+exports.saveWorkout = (workout) => {
+    const db = new sqlite3.Database('team-running.sqlite')
+    let sql = `INSERT INTO workouts (distance)`;
+    db.run(`INSERT INTO workouts (distance) VALUES(?)`, [workout.distance], function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        // get the last insert id
+        console.log(`A row has been inserted with rowid ${this.lastID}`);
+      })
+      db.close()
+
+}

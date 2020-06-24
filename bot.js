307057b0fde76abdb477af6141e2eb9a2f1d0d8f
Zoltan Sisak <zoltan.sisak@gmail.com>
2020-06-24 19:31:11 +0200

    getBotAnswer 0.2

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@ exports.getBotAnswer = (message) => {
     if (message) {
         firstChar = message.substring(0,1)
         if (isDigit(firstChar)) 
-        return "Congratulation for your workout!"
+            return "Congratulation for your workout!"
+        else
+            return "Please send your workout deatils in the following form: distance/time (e.g.: 5.3/29:12)!"
     }
-    else
-        return "Please send your workout deatils in the following form: distance/time (e.g.: 5.3/29:12)!"
 }
 
 const  isDigit = str =>/^\d+$/.test(str)

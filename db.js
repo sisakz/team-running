c705680a6f0b7a2976aa705828b3d55f81ad533f
Zoltan Sisak <zoltan.sisak@gmail.com>
2020-07-03 00:19:01 +0200

    check

--- db.js
+++ db.js
@@ -46,7 +46,8 @@ exports.getStatistic = () => new Promise((resolve, reject) => {
       }
       console.log(`Reset workouts`);
       resolve()
-      db.close() 
+      db.close()
+      // ?
     })
   })
 
